6a11fa8eaf42dcde7eddc6c53d62157d284ea3ac
msdhan <85638556+msdhan@users.noreply.github.com>
2024-10-06 18:03:21 +0100

    wrap up for Sunday

--- Personal practice/Python basics/modules/Python basics_modules.py
+++ Personal practice/Python basics/modules/Python basics_modules.py
@@ -7,25 +7,45 @@
 
 
 #-------------------------------------------------------------------------------------------
-# Call a module (within the same folder)
+# Call a module frmo another file (within the same folder)
 
 # First, change working directory to "modules" folder under "Python basics"
 # Put both this script and "mod1.py" script within the "modules" folder
 # Run in the terminal: cd "C:\Users\handa\OneDrive\Desktop\PhD progress\datawork\Personal practice\Python basics\modules"
 # Run in the terminal: See what's in the directory, run "dir" to ensure I'm in the correct working directory
-# then run the following 
 
+# Then, run the following by running import <module_name>:
 import mod1          # import는 현재 디렉터리에 있는 파일이나 파이썬 라이브러리가 저장된 디렉터리에 있는 모듈만 불러올 수 있다.
-print(mod1.add(3,4)) # returns 7
-print(mod1.sub(5,3)) # returns 2
+a = mod1.add(3,4)
+b = mod1.sub(5,3)
+print(a) # calls add() from mod1.py, returns 7
+print(b) # calls sub() from mod1.py, returns 2
 
+# A cleaner way that allows PARTIAL IMPORT of methods:
+from mod1 import add, sub    # imports add() and sub() from mod1.py
+c = add(3,4)
+d = sub(5,3)
+print(c)  # returns 7
+print(d)  # returns 2
 
-
-
+# Alternative way that imports ALL methods from a module:
+from mod1 import *
+e = add(3,4)
+f = sub(5,3)
+print(e)  # returns 7
+print(f)  # returns 2
 
 
 #-------------------------------------------------------------------------------------------
 # The meaning of [ if __name__ == "__main__" ]
+
+# 모듈 안에 if __name__ == "__main__"을 사용하면 C:\doit>python mod1.py처럼 직접 이 파일을 실행했을 때는
+# __name__ == "__main__"이 참이 되어 if 문 다음 문장이 수행된다. 
+
+# 이와 반대로 대화형 인터프리터나 다른 파일에서 이 모듈을 불러 사용할 때는
+# __name__ == "__main__"이 거짓이 되어 if 문 다음 문장이 수행되지 않는다.
+
+
 
 
 
@@ -35,8 +55,6 @@ print(mod1.sub(5,3)) # returns 2
 
 
 
-#-------------------------------------------------------------------------------------------
-# Call a module from another file (within the same directory)
 
 
 
--- Personal practice/Python basics/modules/mod1.py
+++ Personal practice/Python basics/modules/mod1.py
@@ -4,3 +4,15 @@ def add(a, b):
 
 def sub(a, b): 
     return a-b
+
+## modifications for "The meaning of [ if __name__ == "__main__" ]"
+if __name__ == "__main__":
+    print(add(1, 4))
+    print(sub(4, 2))
+
+# 모듈 안에 if __name__ == "__main__"을 사용하면 C:\doit>python mod1.py처럼 직접 이 파일을 실행했을 때는
+# __name__ == "__main__"이 참이 되어 if 문 다음 문장이 수행된다. 
+
+# 이와 반대로 대화형 인터프리터나 다른 파일에서 이 모듈을 불러 사용할 때는
+# __name__ == "__main__"이 거짓이 되어 if 문 다음 문장이 수행되지 않는다.
+
